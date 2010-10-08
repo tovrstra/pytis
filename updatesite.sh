9c31fc6fc2d8664d0722f96e9682541eb67fb905
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2010-10-08 14:21:48 +0200

    Improved update-site script

--- updatesite.sh
+++ updatesite.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
 (./cleanfiles.sh; ./cleancode.sh; cd doc; make clean; make html)
-cp -ar doc/_build/html ../pytis_html
-git checkout gh-pages && (rm -rf *; cp -ar ../pytis_html/* .; git add .; git commit -m 'Automatic documentation update')
 rm -rf ../pytis_html
-git checkout master
+cp -ar doc/_build/html ../pytis_html
+./cleanfiles.sh
+git checkout gh-pages && (
+    git rm -r *;
+    cp -ar ../pytis_html/* .;
+    mv _static static
+    for f in $(find | grep -v '/.git'); do sed -e 's/_static/static/' -i $f; done
+    mv _sources sources
+    for f in $(find | grep -v '/.git'); do sed -e 's/_sources/sources/' -i $f; done
+    git add .;
+    #git commit -m 'Automatic documentation update'
+)
+#rm -rf ../pytis_html
+#git checkout master
