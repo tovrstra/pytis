5df12889c5159897bd6d26c5a77ff818abe8ec13
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2010-10-08 14:28:28 +0200

    Improved clean script

--- cleanfiles.sh
+++ cleanfiles.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 for i in $(find pytis test examples | egrep "\.pyc$|\.py~$|\.pyc~$|\.bak$|\.so$") ; do rm -v ${i}; done
 rm -vr doc/_build/
+rmdir -v doc/_static/
 rm -vr output
 rm -v MANIFEST
 rm -vr dist
